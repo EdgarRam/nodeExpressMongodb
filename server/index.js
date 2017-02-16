d8e7b4c5eec73a1e03ec3cc31969cdbabe047510
Edgar <tanotuff12@gmail.com>
2017-02-15 22:28:27 -0600

    feat(): se agrega layout pug

--- server/index.js
+++ server/index.js
@@ -3,16 +3,15 @@ var express = require('express')
 
 var app = express();
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
 app.get('/', function( req, res){
   res.render( 'index' )
 })
 
-app.get('/:nombre', function( req, res){
-  console.log(req.params);
-
-  res.send( 'index' )
+app.get('/login', function( req, res){
+  res.render( 'login' )
 })
+
 
 app.listen( 8000)
