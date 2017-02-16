452189cc230e8aaeb13b16d5a8bad45225540391
Edgar <tanotuff12@gmail.com>
2017-02-15 22:57:29 -0600

    feat() configuracion bodyParser

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 var express = require('express')
-var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express();
+
+app.use( '/public', express.static('public') );
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({extended:true}) );
 
 app.set('view engine', 'pug');
 
@@ -11,6 +16,12 @@ app.get('/', function( req, res){
 
 app.get('/login', function( req, res){
   res.render( 'login' )
+})
+
+app.post('/users', function( req, res){
+  console.log(req.body.password);
+  console.log(req.body.email);
+  res.send('datos recibidos')
 })
 
 
